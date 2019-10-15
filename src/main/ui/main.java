9578e63ad42156b1be3769fcf63071f368c89b63
Juan Pablo Garcia <juapgarc@gmail.com>
2019-10-14 21:58:35 -0700

    Compiling Fix and Acomodation of files
    New file acomodation, and fixed the compiling issues, have to refactor most file names to have .java.

---
+++ src/main/ui/main.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
